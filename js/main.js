2ffc314b6ffedaefb944e82b1a19c11ce38556c9
CarlsKe <fernancarldiaz@outlook.com>
2017-07-20 13:41:09 -0500

    feat: added modal image

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById('modalDescription');
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
+var images = document.getElementsByTagName('img');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("content");
+var i;
+for (i = 0; i < images.length; i++) {
+   images[i].onclick = function(){
+       modal.style.display = "block";
+       modalImg.src = this.src;
+       modalImg.alt = this.alt;
+       captionText.innerHTML = this.nextElementSibling.innerHTML;
+   }
+}
